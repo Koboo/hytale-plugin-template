9754f115ef3710a2854019ace4b1b6ad7adde01d
Koboo <admin@koboo.eu>
2026-01-13 22:01:24 +0100

    Create hytale template for gradle

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("java")
+    id("com.gradleup.shadow") version("9.3.1")
+    id("cl.franciscosolis.blossom-extended") version("1.3.1")
+}
+
+group = "REPLACE_WITH_YOUR_GROUP_ID"
+version = "REPLACE_WITH_YOUR_VERSION"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(files("libs/HytaleServer-1.0.0.jar"))
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+}
+
+sourceSets {
+    main {
+        java.setSrcDirs(listOf("src/java"))
+        resources.setSrcDirs(listOf("src/resources"))
+    }
+    test {
+        java.setSrcDirs(emptyList<String>())
+        resources.setSrcDirs(emptyList<String>())
+    }
+}
---
+++ src/java/template/mod/TemplateMod.java
@@ -0,0 +1,22 @@
+package template.mod;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
+
+public class TemplateMod extends JavaPlugin {
+
+    public TemplateMod(@NonNullDecl JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        super.setup();
+    }
+
+    @Override
+    protected void shutdown() {
+        super.shutdown();
+    }
+}
