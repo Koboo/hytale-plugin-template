267a998d3afb497c2cc2aa817208379356029ae4
Koboo <admin@koboo.eu>
2026-01-15 17:13:14 +0100

    Refactor MyPlugin

--- src/java/eu/koboo/myplugin/MyPlugin.java
+++ src/java/eu/koboo/myplugin/MyPlugin.java
@@ -1,22 +1,24 @@
-package your.plugin;
+package eu.koboo.myplugin;
 
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
 
-public class YourPlugin extends JavaPlugin {
+public class MyPlugin extends JavaPlugin {
 
-    public YourPlugin(@NonNullDecl JavaPluginInit init) {
+    public MyPlugin(@NonNullDecl JavaPluginInit init) {
         super(init);
     }
 
     @Override
     protected void setup() {
+        getLogger().atInfo().log("MyPlugin was successfully started!");
         super.setup();
     }
 
     @Override
     protected void shutdown() {
+        getLogger().atInfo().log("MyPlugin was successfully shutdown!");
         super.shutdown();
     }
 }
